fa344d9a70a1a23c76b19a2410be59c66b3ca246
Sanjay Tailor <st.sanjaytailor@gmail.com>
2020-11-22 13:47:31 +0530

    Updates

---
+++ precache-manifest.6073b479d8d7af0a689ed6b90aad24e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139cb2c151519ffa00608aba5e8bdca6",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "543b0f38ffc69b814a70",
+    "url": "/monsters/static/css/main.c7d08ec0.chunk.css"
+  },
+  {
+    "revision": "e0ed94bbd52870c9c783",
+    "url": "/monsters/static/js/2.019902b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.019902b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543b0f38ffc69b814a70",
+    "url": "/monsters/static/js/main.727c94ee.chunk.js"
+  },
+  {
+    "revision": "b4d4b07bf9648c04c9d8",
+    "url": "/monsters/static/js/runtime-main.99233184.js"
+  }
+]);
